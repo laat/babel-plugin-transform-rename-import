ce3f39cf874e47ee156859e31cfcf35b92ca3c6a
Sigurd Fosseng <sigurd@fosseng.net>
2017-06-15 13:36:47 +0200

    Support multiple replacements (#2)
    * Support multiple replacements

--- src/index.js
+++ src/index.js
@@ -9,28 +9,39 @@ function replace(value, original, replacement) {
   return value.replace(pattern, replacement);
 }
 
+function getReplacements(state) {
+  if (state.opts instanceof Array) {
+    return state.opts;
+  }
+  return [state.opts];
+}
+
 export default function visitor({ types: t }) {
   const source = (value, original, replacement) =>
     t.stringLiteral(replace(value, original, replacement));
   return {
     visitor: {
       ImportDeclaration(path, state) {
-        const { original, replacement } = state.opts;
-        const value = path.node.source.value;
-        if (isModule(value, original)) {
-          path.node.source = source(value, original, replacement);
-        }
+        const replacements = getReplacements(state);
+        replacements.forEach(({ original, replacement }) => {
+          const value = path.node.source.value;
+          if (isModule(value, original)) {
+            path.node.source = source(value, original, replacement);
+          }
+        });
       },
 
       CallExpression(path, state) {
-        const { original, replacement } = state.opts;
-        const node = path.node;
-        if (node.callee.name === 'require' &&
-            node.arguments && node.arguments.length === 1 &&
-            t.isStringLiteral(node.arguments[0]) &&
-            isModule(node.arguments[0].value, original)) {
-          path.node.arguments = [source(node.arguments[0].value, original, replacement)];
-        }
+        const replacements = getReplacements(state);
+        replacements.forEach(({ original, replacement }) => {
+          const node = path.node;
+          if (node.callee.name === 'require' &&
+          node.arguments && node.arguments.length === 1 &&
+          t.isStringLiteral(node.arguments[0]) &&
+          isModule(node.arguments[0].value, original)) {
+            path.node.arguments = [source(node.arguments[0].value, original, replacement)];
+          }
+        });
       },
     },
   };
